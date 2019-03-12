2209f54f224bf8c68da75f22fc94ebac89f81f97
malka88 <malka_88@outlook.com>
2019-03-12 12:38:18 +0700

    programm

--- trpoLab5/Program.cs
+++ trpoLab5/Program.cs
@@ -8,6 +8,36 @@ namespace trpoLab5
 {
     class Program
     {
+        static double Conv(float x)
+        {
+            return x*2.54;
+        }
+
+        static bool check (int a)
+        {
+            if (a % 2 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        static int max(int[] array)
+        {
+            int max;
+            max = array[0];
+            for (int i=1; i<array.Length; i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+            }
+
+            return max;
+        }
+
+        static int rem(int a, int b)
+        {
+            return a % b;
+        }
         static void Main(string[] args)
         {
         }
